d0779f2346f8d1aefd13704e81d04d6204bc707d
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-08-06 17:42:55 -0400

    Update home page styling

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,22 +1,58 @@
 import { about } from '../assets/info';
 import portrait from '../assets/portrait.jpg';
-import { Stack, VStack, Image, Text } from '@chakra-ui/react';
+import {
+  Flex,
+  Stack,
+  VStack,
+  Image,
+  Text,
+  List,
+  ListItem,
+  Heading,
+  useColorModeValue,
+} from '@chakra-ui/react';
 
 export default function Home() {
   const { statement, tools, languages, skills } = about;
 
   return (
     <VStack>
-      <Stack direction={['column', 'row']} px={{ base: 4, md: '25%' }}>
+      <Flex
+        direction={{ base: 'column', md: 'row' }}
+        justifyContent='center'
+        alignItems='center'
+        px={{ base: 4, md: '10%', lg: '25%' }}
+        pt={{base: 0, md: 6}}
+        pb={12}>
         <Image
           src={portrait}
           alt='Jason Fox'
           boxSize='128px'
-          m='auto'
           borderRadius='full'
-          objectFit='cover' />
-        <Text textAlign={{ base: 'center', md: 'left' }}>{statement}</Text>
+          objectFit='cover'
+          mr={{ base: 0, md: '1rem' }}
+          style={{ borderStyle: 'solid', borderWidth: '4px', borderColor: useColorModeValue('gray.100', 'gray.800') }} />
+        <Text textAlign={{ base: 'center', md: 'left' }}>
+          {statement}
+        </Text>
+      </Flex>
+      <Stack direction={['column', 'row']} spacing={{ base: 6, md: 16, lg: 36 }}>
+        <SkillList name='Skills' list={skills} />
+        <SkillList name='Languages' list={languages} />
+        <SkillList name='Tools' list={tools} />
       </Stack>
     </VStack>
+  )
+}
+
+function SkillList({ name, list }) {
+  return (
+    <List textAlign={{ base: 'center', md: 'left' }}>
+      <Heading as='h3' size='xl' pb={1}>{name}</Heading>
+      {list.map(item =>
+        <ListItem key={item}>
+          {item}
+        </ListItem>)}
+    </List>
   )
 }
