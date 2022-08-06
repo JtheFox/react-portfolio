7c357b40984c8881aaa00ad3038a47e69b6c3655
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-08-06 15:30:20 -0400

    Create footer layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,27 @@
-import { HStack } from '@chakra-ui/react';
+import { Flex, Icon, Link, Tooltip } from '@chakra-ui/react';
 import { links } from '../assets/info';
 
-function Footer() {
+export default function Footer() {
   return (
     <footer>
-      <HStack>
-        
-      </HStack>
+      <Flex justifyContent='center' spacing='1rem'>
+        {links.map(({ name, desc, url, active, icon }) => {
+          return (
+            <Tooltip label={desc} placement='top'>
+              <Link href={url} p={3}>
+                <Icon
+                  key={name}
+                  as={icon}
+                  w={8}
+                  h={8}
+                >
+                </Icon>
+              </Link>
+            </Tooltip>
+          )
+        })}
+      </Flex>
     </footer>
   )
 }
+
