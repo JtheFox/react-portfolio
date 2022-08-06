e7cc6772f8c49c585b0daebbf0d46aea25f8b6cf
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-08-06 15:55:12 -0400

    Create navbar layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,24 @@
-import { Flex, Spacer } from '@chakra-ui/react';
+import { Heading, Flex, Spacer, HStack } from '@chakra-ui/react';
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
+  let activeStyle = {
+    textDecoration: "underline",
+  };
+
   return (
-    <Flex as='nav'>
-      
+    <Flex as='header'>
+      <Heading as='h1'>Jason Fox</Heading>
+      <Spacer />
+      <HStack as='nav'>
+        <NavLink
+          to='/'
+          style={({ isActive }) =>
+            isActive ? activeStyle : undefined
+          }>
+          About
+        </NavLink>
+      </HStack>
     </Flex>
   )
 }
